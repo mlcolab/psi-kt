eed772d493cfaa71cb64bf414e66e1fec260e49c
zhouhq10 <hzhou52@bg-slurmb-login1.novalocal>
2023-01-30 03:18:09 +0100

    data

--- data/data_loader.py
+++ data/data_loader.py
@@ -10,6 +10,14 @@ import numpy as np
 import pandas as pd
 import ipdb
 
+
+
+        
+
+
+
+
+
 class DataReader(object):
     
     def __init__(self, args, logs):
@@ -31,16 +39,35 @@ class DataReader(object):
         # aggregate by user
         user_wise_dict = dict()
         cnt, n_inters = 0, 0
-        
+
         for user, user_df in self.inter_df.groupby('user_id'):
+
             df = user_df[:self.max_step]  # consider the first 50 interactions
+            skill_unique = df.skill_id.unique()
+            sk_dfs = []
+            for sk in skill_unique:
+                sk_df = df[df.skill_id==sk].copy()
+                sk_df['num_history'] = list(np.arange(len(sk_df)))
+                num_correct = 0
+                for i in range(len(sk_df)):
+                    sk_df.loc[sk_df.index[i], 'num_success'] = num_correct
+                    if sk_df.loc[sk_df.index[i], 'correct'] > 0:
+                        num_correct += 1
+                sk_df['num_failure'] = sk_df.num_history - sk_df.num_success
+                sk_dfs.append(sk_df)
+            new_df = pd.concat(sk_dfs, 0).sort_values('timestamp', ascending=True)
+
             user_wise_dict[cnt] = {
                 'user_id': user,
-                'skill_seq': df['skill_id'].values.tolist(), 
+                'skill_seq': new_df['skill_id'].values.tolist(), 
                 'correct_seq': [round(x) for x in df['correct']],
-                'time_seq': df['timestamp'].values.tolist(),
-                'problem_seq': df['problem_id'].values.tolist()
+                'time_seq': new_df['timestamp'].values.tolist(),
+                'problem_seq': new_df['problem_id'].values.tolist(),
+                'num_history': new_df['num_history'].values.astype(int).tolist(),
+                'num_success': new_df['num_success'].values.astype(int).tolist(),
+                'num_failure': new_df['num_failure'].values.astype(int).tolist(),
             }
+            
             cnt += 1
             n_inters += len(df)
 
@@ -48,8 +75,6 @@ class DataReader(object):
         self.n_users = max(self.inter_df['user_id'].values) + 1
         self.n_skills = max(self.inter_df['skill_id']) + 1
         # TODO: now, every user list is not in the same length. Add 0 to make them all in max_step length
-        # ipdb.set_trace()
-        # self.n_skills = 124 # ass0910 tmp TODO because use different data from hkt and skt # max(self.inter_df['skill_id']) + 1
         # TODO the number of unique exercise in log and exercise is not the same
         # self.inter_df['exercise'].unique().shape[0] # max(self.inter_df['skill_id']) + 1
         self.n_problems = max(self.inter_df['problem_id']) + 1
@@ -62,8 +87,12 @@ class DataReader(object):
             self.n_users, self.n_skills, self.n_problems, n_inters
         ))
 
+
     def gen_fold_data(self, k):
-        # TODO randomize the index
+        '''
+        Args:
+            k: select the k-th fold to run
+        '''
         assert k < self.k_fold
         n_examples = len(self.user_seq_df)
         fold_size = math.ceil(n_examples / self.k_fold)
--- data/synthetic_gen.py
+++ data/synthetic_gen.py
@@ -9,52 +9,66 @@ import networkx as nx
 import scipy
 from scipy import stats
 
+import os
 import argparse
-import torch
 import datetime
+import torch
 
-from data.ou_process import VanillaOU, VanillaGraphOU, ExtendGraphOU
+from utils.parametric_models import VanillaOU, GraphOU, ExtendGraphOU
 from utils.visualize import *
+f# rom data.data_loader import KTData
 
 import ipdb
 
 def parse_args(parser):
     # ----- global -----
-    parser.add_argument('--random_seed', type=int, default=1,)
+    parser.add_argument('--random_seed', type=int, default=8,)
     parser.add_argument('--num_sequence', type=int, default=100,)
+    parser.add_argument('--learner_model', type=str, default='ou_graph', help=['hlr', 'ou', 'ou_graph', 'ou_extend_graph', 'ppe'])
+    
+    # ----- time points -----
+    parser.add_argument('--time_random_type', type=str, default='random', hel=['random', 'uniform'])
+    parser.add_argument('--time_step', type=int, default=50,)
+    parser.add_argument('--max_time_step', type=int, default=500,)
 
     # ----- random graph -----
-    parser.add_argument('--num_node', type=int, default=5,)
+    parser.add_argument('--num_node', type=int, default=3,)
     parser.add_argument('--edge_prob', type=float, default=0.2,)
 
     # ----- ou process -----
     parser.add_argument('--mean_rev_speed', type=float, default=0.02,)
-    parser.add_argument('--mean_rev_level', type=float, default=0.5,)
+    parser.add_argument('--mean_rev_level', type=float, default=0.7,)
     parser.add_argument('--mean_base_level', type=float, default=1.0,)
-    parser.add_argument('--vola', type=float, default=0.08,)
+    parser.add_argument('--vola', type=float, default=0.01,)
 
+    
 
-    parser.add_argument('--time_step', type=int, default=50,)
-    parser.add_argument('--max_time_step', type=int, default=350,)
-    parser.add_argument('--random_type', type=str, default='random',)
+    # ----- save path -----
+    parser.add_argument('--save_path', type=str, default='/mnt/qb/work/mlcolab/hzhou52/kt/synthetic',)
 
     return parser 
 
-def save_as_unified_format(args, path, times, save_path):
+
+class KTData(object):
+    def __init__(self, data_type='syn', df=None):
+        pass
+    def init_synthetic_data(self):
+        pass
+    def init_real_data(self):
+        pass
+
+
+
+
+
+
+def save_as_unified_format(args, path, times, items, adj):
+
     df = []
     timestamp = times.flatten()
     dwell_time = np.zeros_like(timestamp)
 
-    rng = default_rng(args.random_seed)
-    items = []
-    p_items = []
-    for i in range(args.num_sequence):
-        item = rng.choice(np.arange(args.num_node), args.time_step, True)
-        items.append(item)
-        p_items.append(path[i, np.arange(args.time_step), item]) 
-    items = np.stack(items)
-    p_items = np.stack(p_items).flatten()
-    correct = (sigmoid(p_items) >= 0.5)*1
+    correct = (p_items >=0.5) * 1 # (sigmoid(p_items) >= 0.5)*1 # TODO???
 
     problem_id = items.flatten()
     skill_id = items.flatten()
@@ -64,7 +78,6 @@ def save_as_unified_format(args, path, times, save_path):
     df = np.stack([timestamp, dwell_time, correct, problem_id, skill_id, user_id], -1)
     df = pd.DataFrame(df, columns=['timestamp', 'dwell_time', 'correct', 'problem_id', 'skill_id', 'user_id'])
 
-    ipdb.set_trace()
     df = df.astype({
     'timestamp': np.float64,
     'dwell_time': np.float64,
@@ -75,16 +88,23 @@ def save_as_unified_format(args, path, times, save_path):
     })
 
     # Save
-    df.to_csv('/mnt/qb/work/mlcolab/hzhou52/kt/synthetic/'+'interactions_{}.csv'.format(args.time_step), sep='\t', index=False)
+    if not os.path.exists(args.log_path):
+        os.makedirs(args.log_path)
+    adj_path = os.path.join(args.log_path, 'adj.npy')
+    np.save(adj_path, adj)
+    df_path = os.path.join(args.log_path, 'interactions_{}.csv'.format(args.time_step))
+    df.to_csv(df_path, sep='\t', index=False)
 
-    # return df
 
 def time_point_generate(args):
-    if args.random_type == 'uniform':
+    '''
+    
+    '''
+    if args.time_random_type == 'uniform':
         times = np.arange(0, args.max_time_step, args.max_time_step//args.time_step)
         times = np.tile(np.expand_dims(times, 0), (args.num_sequence, 1)) # [num_deq, time_step]
         
-    elif args.random_type == 'random':
+    elif args.time_random_type == 'random':
         rng = default_rng(args.random_seed)
         times = []
         for i in range(args.num_sequence):
@@ -92,43 +112,63 @@ def time_point_generate(args):
             time.sort()
             times.append(time)
         times = np.stack(times)
+
     return times
 
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-def review_item_generate():
-    pass
-
-
-
-def main(args, graph):
-    pass
+def review_item_generate(args):
+    rng = default_rng(args.random_seed)
+    items = []
+    # p_items = []
+    for i in range(args.num_sequence):
+        item = rng.choice(np.arange(args.num_node), args.time_step, True)
+        items.append(item)
+        # p_items.append(path[i, np.arange(args.time_step), item]) 
+    items = np.stack(items)
+    # p_items = np.stack(p_items).flatten()
+    ipdb.set_trace()
+    return items
 
 
 if __name__ == '__main__':
     # ----- args -----
     parser = argparse.ArgumentParser(description='Global')
     parser = parse_args(parser)
+    
+    global args
     args, extras = parser.parse_known_args()
 
     args.time = datetime.datetime.now().isoformat()
     args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    args.log_path = os.path.join(args.save_path, args.time + '_' + 'node_' + str(args.num_node) + 'mean_' + str(args.mean_rev_level) + \
+                                    'speed_' + str(args.mean_rev_speed) + 'var_' + str(args.vola))
+    
+    
+
+
+
+
+
+    # -- generate random graphs
     graph = nx.erdos_renyi_graph(args.num_node, args.edge_prob, seed=args.random_seed, directed=True)
     adj = nx.adjacency_matrix(graph).toarray()
-    np.save('/mnt/qb/work/mlcolab/hzhou52/kt/synthetic/adj.npy', adj)
-    ipdb.set_trace()
+    # draw_graph(graph, args)
+
+    # -- generate random (or uniform) time points
     times = time_point_generate(args)
 
-    ou_generator = VanillaGraphOU(args.mean_rev_speed, args.mean_rev_level, args.vola, args.num_sequence, graph)
+    # -- generate reviewing items
+    items = review_item_generate(args)
+
+
+    # -- OU process 
+    ou_generator = GraphOU(args.mean_rev_speed, args.mean_rev_level, args.vola, args.num_sequence, graph) # test
     path = ou_generator.simulate_path(np.zeros((args.num_node,1)), times)
 
-    save_as_unified_format(args, path, times, save_path=None)
-    ipdb.set_trace()
-    # # TODO debug: compare with vanilla
-    # ou_generator = VanillaGraphOU(args.mean_rev_speed, args.mean_rev_level, args.vola, graph)
-    # path = ou_generator.simulate_path(np.zeros((args.num_node,1)), times)
+    save_as_unified_format(args, path, times, items, adj)
 
     ou_vanilla_generator = VanillaOU(args.mean_rev_speed, args.mean_rev_level, args.vola, args.num_sequence)
     vanilla_path = ou_vanilla_generator.simulate_path(np.zeros((args.num_node,1)), times)
@@ -137,8 +177,8 @@ if __name__ == '__main__':
     draw_path(path[0], args, times[0], prefix='graph')
     draw_path(vanilla_path[0], args, times[0], prefix='vanilla')
     draw_path(sigmoid(path[0]), args, times[0], prefix='sigmoid')
-    draw_graph(graph)
-    
+    visualize_ground_truth(graph, args, adj)
 
-    # ipdb.set_trace()
+    ipdb.set_trace()
+    
     

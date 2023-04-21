2181781ac4853c4a7d9d69ea775d2efe97a02578
hzhou <zzzzzhq@outlook.com>
2023-04-21 12:33:18 +0200

    exp

--- exp/baseline.sh
+++ exp/baseline.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+#SBATCH --ntasks=1                # Number of tasks (see below)
+#SBATCH --cpus-per-task=1         # Number of CPU cores per task
+#SBATCH --nodes=1                 # Ensure that all cores are on one machine
+#SBATCH --time=0-12:00            # Runtime in D-HH:MM
+#SBATCH --partition=gpu-v100
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=64G  
+#SBATCH --output=hostname_%j.out  # File to which STDOUT will be written
+#SBATCH --error=hostname_%j.err   # File to which STDERR will be written
+#SBATCH --mail-type=END           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --array=0
+
+# python exp_baseline.py --dataset assistment12/multi_skill --max_step 50 \
+# --model_name DKTForgetting --load 0 \
+# --gpu 0 \
+# --epoch 100 --vcl 0 \
+# --train_mode simple_split_time --overfit 16 \
+# --batch_size 256 \
+# --validate 1 --validate_every 1 --save_every 5
 
 python exp_baseline.py --dataset assistment12/multi_skill --max_step 50 \
 --model_name DKT --load 0 \
---lr 5e-3 --l2 1e-5 --gpu 0 \
---epoch 2 --vcl 1 \
---train_mode simple_split_time --overfit 16 \
---batch_size 16 \
---validate 1 --validate_every 1
-# --graph_params [["correct_transition_graph.json", True]]
+--gpu 0 \
+--epoch 100 --vcl 1 \
+--train_mode simple_split_time --overfit 0 \
+--batch_size 256 \
+--validate 1 --validate_every 1 --save_every 5

4ddb5cd5762fb6c1e05a39c43d20d68e6172a34c
hzhou <zzzzzhq@outlook.com>
2023-06-01 16:35:16 +0200

    Simplify input args

--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -10,7 +10,6 @@ def parse_args(parser):
     '''
     ############## TODO debugging experiment ############## 
     parser.add_argument('--overfit', type=int, default=100, help='whether to overfit and debug (0/>0); if overfit>0, the number of data to train')
-    parser.add_argument('--gt_adj_path', type=str)
 
     ############## distributed training ############## 
     parser.add_argument('--world_size', default=-1, type=int, help='number of nodes for distributed training')
@@ -28,13 +27,12 @@ def parse_args(parser):
     ############## data loader ##############
     parser.add_argument('--data_dir', type=str, default='/mnt/qb/work/mlcolab/hzhou52/kt', help='Input data dir.')
     parser.add_argument('--dataset', type=str, help='[junyi, assistment12, ]')
-    parser.add_argument('--sep', type=str, default='\t', help='sep of csv file.')
     parser.add_argument('--kfold', type=int, default=5, help='K-fold number.')
     parser.add_argument('--max_step', type=int, default=50, help='Max time steps per sequence.')
     parser.add_argument('--regenerate_corpus', action="store_true", default=False)
 
     ############## logger ##############
-    parser.add_argument("--save_folder", type=str, default="/mnt/qb/work/mlcolab/hzhou52/kt/logs",)
+    parser.add_argument("--save_folder", type=str, default="/mnt/qb/work/mlcolab/hzhou52/Exp3_continual",)
     parser.add_argument("--save_every", type=int, default=10,)
     parser.add_argument("--expername", type=str, default="",)
 
@@ -44,16 +42,15 @@ def parse_args(parser):
     parser.add_argument('--train_time_ratio', type=float, default=0.4, help='')
     parser.add_argument('--test_time_ratio', type=float, default=0.5, help='')
 
-    parser.add_argument('--train', type=int, default=1, help='To train the model or not.')
     parser.add_argument(
-                '--train_mode', type=str, default='train_split_time', 
+                '--train_mode', type=str, default='simple_split_time', 
                 help= 'simple_split_time' + 'simple_split_learner' 
                 + 'ls_split_time' 
                 + 'ns_split_time' + 'ns_split_learner'
                 + 'ln_split_time', 
             ) #
+    parser.add_argument("--validate", default=1, type=int, help="validate results throughout training.")
     parser.add_argument("--test", default=0, type=int, help="test results throughout training.")
-    parser.add_argument("--validate", default=0, type=int, help="test results throughout training.")
     parser.add_argument("--test_every", type=int, default=5, help="test results throughout training.")
     parser.add_argument('--epoch', type=int, default=200, help='Number of epochs.')
     parser.add_argument('--early_stop', type=int, default=1, help='whether to early-stop.')
@@ -63,11 +60,10 @@ def parse_args(parser):
     parser.add_argument('--dropout', type=float, default=0.0, help='Dropout probability for each deep layer')
     parser.add_argument('--l2', type=float, default=1e-5, help='Weight of l2_regularize in loss.')
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer: GD, Adam, Adagrad, Adadelta')
-    parser.add_argument('--metric', type=str, default='F1, Accuracy, Recall, Precision, AUC', 
+    parser.add_argument('--metric', type=str, default='Accuracy, F1, Recall, Precision, AUC', 
                         help='metrics: AUC, F1, Accuracy, Recall, Presicion;'
                                 'The first one will be used to determine whether to early stop')
     parser.add_argument('--fold', type=int, default=0, help='Select a fold to run.')
-    
     
     ############## load and save model ##############
     parser.add_argument('--load', type=int, default=0, help='Whether load model and continue to train')
@@ -79,10 +75,8 @@ def parse_args(parser):
         + "Leave empty to train from scratch",
     )
     
-
     ############## training hyperparameter ##############
     parser.add_argument("--lr_decay", type=int, default=5000, help="After how epochs to decay LR by a factor of gamma.",)
     parser.add_argument("--gamma", type=float, default=0.5, help="LR decay factor.")
-    parser.add_argument("--var", type=float, default=5e-2, help="Output variance.") # TODO
     
     return parser 

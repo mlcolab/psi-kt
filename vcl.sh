98804c5badd8559feca73ce3892d9582a256cc5f
hzhou <zzzzzhq@outlook.com>
2023-06-01 16:27:51 +0200

    Delete unnecessary files

--- vcl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python main.py --dataset assistment12 \
---model_name CausalKT --load 0 \
---max_step 50 --lr 5e-3 --l2 1e-5 --time_log 5 --gpu 0 \
---epoch 200 --emb_history 1 --overfit 0 \
---num_graph 10 --decoder_type oudebug \
---latent_rep basic --batch_size 32 # --regenerate_corpus
-# --graph_params [["correct_transition_graph.json", True]]

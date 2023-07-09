8227a89f5e11c1f6af00aa2b85d208bff9933f7c
hzhou <zzzzzhq@outlook.com>
2023-07-09 22:53:10 +0200

    Draft different train_val split.

--- data/data_loader.py
+++ data/data_loader.py
@@ -3,6 +3,7 @@ import math
 import numpy as np
 import pandas as pd
 import ipdb
+import pickle
 
 
 class DataReader(object):
@@ -29,16 +30,27 @@ class DataReader(object):
         self.dataset = args.dataset
         self.k_fold = args.kfold
         self.max_step = int(args.max_step)
+        self.overfit  = args.overfit
+        
+        self.train_mode = args.train_mode
+        self.train_time_ratio = args.train_time_ratio
+        self.test_time_ratio = args.test_time_ratio
+        self.val_ratio = args.val_time_ratio
+        
+        self.args = args
         self.logs = logs
         
-        self.inter_df = pd.read_csv(os.path.join(self.data_dir, self.dataset, 'interactions_{}.csv'.format(args.max_step)), sep='\t')
+        self.inter_df = pd.read_csv(os.path.join(self.data_dir, self.dataset, 'interactions_{}.csv'.format(self.max_step)), sep='\t')
+        self.corpus_path = os.path.join(self.data_dir, self.dataset, 'Corpus_{}.pkl'.format(self.max_step))
+        
+        
+    def create_corpus(self):
+        '''
+        '''
         if 'problem_id' not in self.inter_df.columns:
             self.inter_df['problem_id'] = self.inter_df['skill_id']
-
-        #  load the ground-truth graph if available  
-        self.adj = self.load_ground_truth_graph()
         
-        logs.write_to_log_file('Reading data from \"{}\", dataset = \"{}\" '.format(self.data_dir, self.dataset))
+        self.logs.write_to_log_file('Reading data from \"{}\", dataset = \"{}\" '.format(self.data_dir, self.dataset))
 
         # Aggregate by user
         user_wise_dict = dict()
@@ -76,6 +88,7 @@ class DataReader(object):
             cnt += 1
             n_inters += len(df)
         
+        ipdb.set_trace()
         self.user_seq_df = pd.DataFrame.from_dict(user_wise_dict, orient='index')
         self.n_users = max(self.inter_df['user_id']) + 1
         self.n_skills = max(self.inter_df['skill_id']) + 1
@@ -84,6 +97,12 @@ class DataReader(object):
         self.logs.write_to_log_file('"n_users": {}, "n_skills": {}, "n_problems": {}, "n_interactions": {}'.format(
             self.n_users, self.n_skills, self.n_problems, n_inters
         ))
+        
+        self.logs.write_to_log_file('Save corpus to {}'.format(self.corpus_path))
+        pickle.dump(self, open(self.corpus_path, 'wb'))
+        
+        #  load the ground-truth graph if available  
+        self.adj = self.load_ground_truth_graph()
 
 
     def load_ground_truth_graph(self):
@@ -152,7 +171,8 @@ class DataReader(object):
         
         train_size = math.ceil(n_time_steps * train_ratio)
         test_size = math.ceil(n_time_steps * test_ratio)
-        val_size = n_time_steps-train_size-test_size
+        val_size = math.ceil(n_time_steps * val_ratio)# n_time_steps-train_size-test_size
+        whole_size = train_size + test_size + val_size
         
         for key in self.user_seq_df.keys():
             if key != 'user_id':
@@ -160,13 +180,54 @@ class DataReader(object):
                 self.data_df['train'][key] = value[:, :train_size].tolist()
                 self.data_df['val'][key] = value[:, train_size:val_size+train_size].tolist()
                 self.data_df['test'][key] = value[:, train_size+val_size:val_size+train_size+test_size].tolist()
-                self.data_df['whole'][key] = value[:, :].tolist()
+                self.data_df['whole'][key] = value[:, :whole_size].tolist()
 
         for key in self.data_df.keys():     
             self.data_df[key] = pd.DataFrame.from_dict(self.data_df[key], orient='columns')   
             self.data_df[key]['user_id'] = self.user_seq_df['user_id']
 
         
+    def gen_time_split_data_improve(self):
+        '''
+        '''
+        ipdb.set_trace()
+        self.data_df = {
+            'train': dict(),
+            'val': dict(),
+            'test': dict(),
+            'whole': dict(),
+        }
+        
+        n_learners = len(self.user_seq_df)
+        
+        assert self.overfit*(1+self.val_ratio) <= n_learners
+        n_val_learners = int(self.overfit * self.val_ratio)
+        train_user_list = self.user_seq_df.sample(n=n_val_learners + self.overfit)
+        val_user_list = train_user_list.sample(n=n_val_learners)
+
+        n_time_steps = len(self.user_seq_df['time_seq'][0])
+        train_time_size = math.ceil(n_time_steps * self.train_time_ratio)
+        test_time_size = math.ceil(n_time_steps * self.test_time_ratio)
+        whole_time_size = train_time_size + test_time_size
+        
+        for key in self.user_seq_df.keys():
+            if key != 'user_id':
+                value = np.stack(train_user_list[key].values)
+                self.data_df['train'][key] = value[:, :train_time_size].tolist()
+                self.data_df['test'][key] = value[:, :whole_time_size].tolist()
+                self.data_df['whole'][key] = value[:, :whole_time_size].tolist()
+                value = np.stack(val_user_list[key].values)
+                self.data_df['val'][key] = value[:, :whole_time_size].tolist()
+            else:
+                self.data_df['train'][key] = train_user_list['user_id'] 
+                self.data_df['test'][key] = train_user_list['user_id']
+                self.data_df['whole'][key] = train_user_list['user_id']
+                self.data_df['val'][key] = val_user_list['user_id']
+                
+        for key in self.data_df.keys():     
+            self.data_df[key] = pd.DataFrame.from_dict(self.data_df[key], orient='columns')   
+        
+            
     def show_columns(self):
         """
         Prints a random row of the user sequence DataFrame to show the available data columns.
@@ -176,4 +237,35 @@ class DataReader(object):
         self.logs.write_to_log_file(self.user_seq_df.iloc[np.random.randint(0, len(self.user_seq_df))])
 
 
+    def load_corpus(self):
+        '''
+        Load corpus from the corpus path, and split the data into k folds. 
 
+        Args:
+            logs: An object to write logs to.
+            args: An object that contains command-line arguments.
+
+        Returns:
+            The corpus object that contains the loaded data.
+        '''
+        
+        # Load the corpus object from the pickle file at the specified path.
+        self.logs.write_to_log_file(f"Load corpus from {self.corpus_path}")
+        with open(self.corpus_path, 'rb') as f:
+            corpus = pickle.load(f)
+        
+        # Check the value of the train_mode argument to determine the type of data split.
+        if 'split_learner' in self.train_mode:
+            corpus.gen_fold_data(self.k_fold)
+            self.logs.write_to_log_file('# Training mode splits LEARNER')
+
+        elif 'split_time' in self.train_mode:
+            # corpus.gen_time_split_data(args.train_time_ratio, args.test_time_ratio, args.val_time_ratio*args.validate)
+            corpus.gen_time_split_data_improve()
+            self.logs.write_to_log_file('# Training mode splits TIME')
+            
+        self.logs.write_to_log_file('# Train: {}, # val: {}, # Test: {}'.format(
+                len(corpus.data_df['train']), len(corpus.data_df['val']), len(corpus.data_df['test'])
+            ))
+
+        return corpus

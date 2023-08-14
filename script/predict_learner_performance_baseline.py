0deb3ba418cb5e8e71ec49b50fab1342a01ec525
zhouhq10 <hzhou52@bg-slurmb-login1.novalocal>
2023-08-14 12:06:21 +0200

    Add environment file and change imported packages after reorganization

--- script/predict_learner_performance_baseline.py
+++ script/predict_learner_performance_baseline.py
@@ -16,7 +16,7 @@ from knowledge_tracing.runner import runner_baseline
 # from VCLRunner_baseline import BaselineVCLRunner
 # from FTRunner_baseline import FTRunner
 from knowledge_tracing.utils import utils, arg_parser, logger
-from knowledge_tracing.baseline import DKT# , DKTForgetting, HKT, AKT, HLR, PPE
+from knowledge_tracing.baseline import DKT, DKTForgetting, HKT, AKT, HLR, PPE
 
 # TODO: this is duplicate with the one in exp
 

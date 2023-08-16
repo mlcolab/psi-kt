7615fb10af03b125ab4f04163e217bc8ea089238
zhouhq10 <hzhou52@bg-slurmb-login1.novalocal>
2023-08-16 19:45:24 +0200

    Refactor the code

--- script/exp_learner_predict.py
+++ script/exp_learner_predict.py
@@ -1,8 +1,9 @@
 # -*- coding: UTF-8 -*-
 import sys
-sys.path.append('..')
 
-import os 
+sys.path.append("..")
+
+import os
 import time
 import pickle
 import argparse
@@ -14,6 +15,7 @@ import torch
 from torch.autograd import profiler
 
 from data import data_loader
+
 # from models import *
 # from KTRunner_test import *
 import KTRunner
@@ -28,51 +30,62 @@ from models.learner_hssm_vcl_model import GraphContinualHSSM
 # from torch.nn.parallel import DistributedDataParallel as DDP
 
 import ipdb
+
 # https://pytorch.org/tutorials/intermediate/ddp_tutorial.html
 
 
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # ----- add aditional arguments for this exp. -----
-    parser = argparse.ArgumentParser(description='Global')
-    parser.add_argument('--model_name', type=str, help='Choose a model to run.')
-    
+    parser = argparse.ArgumentParser(description="Global")
+    parser.add_argument("--model_name", type=str, help="Choose a model to run.")
+
     # debug
-    parser.add_argument('--alpha_minimum', type=float, default=100)
-    parser.add_argument('--learned_graph', type=str, default='w_gt')
+    parser.add_argument("--alpha_minimum", type=float, default=100)
+    parser.add_argument("--learned_graph", type=str, default="w_gt")
 
     # Training options
-    parser.add_argument('--multi_node', type=int, default=0)
-    parser.add_argument('--graph_path', type=str, default='/mnt/qb/work/mlcolab/hzhou52/kt/junyi15/adj.npy')
-    parser.add_argument('--num_sample', type=int, default=10)
-    
+    parser.add_argument("--multi_node", type=int, default=0)
+    parser.add_argument(
+        "--graph_path",
+        type=str,
+        default="/mnt/qb/work/mlcolab/hzhou52/kt/junyi15/adj.npy",
+    )
+    parser.add_argument("--num_sample", type=int, default=10)
+
     parser = arg_parser.parse_args(parser)
-    
-    global_args, extras = parser.parse_known_args() 
+
+    global_args, extras = parser.parse_known_args()
     global_args.time = datetime.datetime.now().isoformat()
     global_args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+
     logs = logger.Logger(global_args)
-    
+
     # ----- data part -----
-    corpus_path = os.path.join(global_args.data_dir, global_args.dataset, 'Corpus_{}.pkl'.format(global_args.max_step))
+    corpus_path = os.path.join(
+        global_args.data_dir,
+        global_args.dataset,
+        "Corpus_{}.pkl".format(global_args.max_step),
+    )
     data = data_loader.DataReader(global_args, logs)
     if not os.path.exists(corpus_path) or global_args.regenerate_corpus:
         data.create_corpus()
-        data.show_columns() 
-    corpus = data.load_corpus() 
-    
+        data.show_columns()
+    corpus = data.load_corpus()
+
     # ----- logger information -----
-    log_args = [global_args.model_name, global_args.dataset, str(global_args.random_seed)]
-    logs.write_to_log_file('-' * 45 + ' BEGIN: ' + utils.get_time() + ' ' + '-' * 45)
+    log_args = [
+        global_args.model_name,
+        global_args.dataset,
+        str(global_args.random_seed),
+    ]
+    logs.write_to_log_file("-" * 45 + " BEGIN: " + utils.get_time() + " " + "-" * 45)
     logs.write_to_log_file(utils.format_arg_str(global_args, exclude_lst=[]))
-    
+
     # ----- random seed -----
     torch.manual_seed(global_args.random_seed)
     torch.cuda.manual_seed(global_args.random_seed)
     np.random.seed(global_args.random_seed)
-    
+
     # ----- GPU & CUDA -----
     if global_args.device.type != "cpu":
         if global_args.GPU_to_use is not None:
@@ -84,119 +97,130 @@ if __name__ == '__main__':
         global_args.batch_size_multiGPU = global_args.batch_size
     logs.write_to_log_file("# cuda devices: {}".format(torch.cuda.device_count()))
     # ipdb.set_trace()
-    
+
     # ----- Model initialization -----
-    if 'ls_' or 'ln_' in global_args.train_mode:
+    if "ls_" or "ln_" in global_args.train_mode:
         num_seq = corpus.n_users
-    else: num_seq = 1
-    
+    else:
+        num_seq = 1
+
     adj = np.load(global_args.graph_path)
-    
-    if global_args.model_name == 'HLR':
+
+    if global_args.model_name == "HLR":
         model = HLR(
-            mode=global_args.train_mode, 
+            mode=global_args.train_mode,
             num_seq=num_seq,
             num_node=1 if not global_args.multi_node else corpus.n_skills,
             nx_graph=None if not global_args.multi_node else adj,
-            device=global_args.device, 
+            device=global_args.device,
             logs=logs,
         )
-        
-    elif global_args.model_name == 'VanillaOU':
+
+    elif global_args.model_name == "VanillaOU":
         model = VanillaOU(
-            mode=global_args.train_mode, 
+            mode=global_args.train_mode,
             num_seq=num_seq,
             num_node=1 if not global_args.multi_node else corpus.n_skills,
             nx_graph=None if not global_args.multi_node else adj,
-            device=global_args.device, 
-            logs=logs
+            device=global_args.device,
+            logs=logs,
         )
-        
-    elif global_args.model_name == 'GraphOU':
+
+    elif global_args.model_name == "GraphOU":
         model = GraphOU(
-            mode=global_args.train_mode, 
+            mode=global_args.train_mode,
             num_seq=num_seq,
             num_node=1 if not global_args.multi_node else corpus.n_skills,
             nx_graph=None if not global_args.multi_node else adj,
-            device=global_args.device, 
-            logs=logs
+            device=global_args.device,
+            logs=logs,
         )
-        
-    elif global_args.model_name == 'PPE':
+
+    elif global_args.model_name == "PPE":
         model = PPE(
-            mode=global_args.train_mode, 
+            mode=global_args.train_mode,
             num_seq=num_seq,
             num_node=1 if not global_args.multi_node else corpus.n_skills,
             nx_graph=None if not global_args.multi_node else adj,
-            device=global_args.device, 
-            logs=logs
+            device=global_args.device,
+            logs=logs,
         )
-        
-    elif global_args.model_name == 'SwitchingNLDS':
+
+    elif global_args.model_name == "SwitchingNLDS":
         model = create_model(
             dim_s=3,
             dim_z=1,
             dim_y=1,
-            device=global_args.device, 
+            device=global_args.device,
             args=global_args,
-            logs=logs,   
+            logs=logs,
         )
-    
+
     # elif global_args.model_name == 'VanillaHSSM':
     #     model = VanillaHSSM( # TestHierachicalSSM(
-    #         mode=global_args.train_mode, 
+    #         mode=global_args.train_mode,
     #         num_seq=num_seq,
     #         num_node=1 if not global_args.multi_node else corpus.n_skills,
     #         nx_graph=None if not global_args.multi_node else adj,
-    #         device=global_args.device, 
+    #         device=global_args.device,
     #         args=global_args,
     #         logs=logs,
     #     )
     #     shutil.copy('/home/mlcolab/hzhou52/knowledge_tracing/models/learner_hssm_model.py',
     #                     global_args.log_path)
-    elif global_args.model_name == 'TestHierachicalSSM' or global_args.model_name == 'GraphHSSM':
-        
+    elif (
+        global_args.model_name == "TestHierachicalSSM"
+        or global_args.model_name == "GraphHSSM"
+    ):
         if global_args.vcl == 0:
-            model = GraphHSSM( # TestHierachicalSSM(
-                mode=global_args.train_mode, 
+            model = GraphHSSM(  # TestHierachicalSSM(
+                mode=global_args.train_mode,
                 num_seq=num_seq,
                 num_node=1 if not global_args.multi_node else corpus.n_skills,
                 nx_graph=None if not global_args.multi_node else adj,
-                device=global_args.device, 
+                device=global_args.device,
                 args=global_args,
                 logs=logs,
             )
         else:
             model = GraphContinualHSSM(
-                mode=global_args.train_mode, 
+                mode=global_args.train_mode,
                 num_seq=num_seq,
                 num_node=1 if not global_args.multi_node else corpus.n_skills,
                 nx_graph=None if not global_args.multi_node else adj,
-                device=global_args.device, 
+                device=global_args.device,
                 args=global_args,
                 logs=logs,
             )
-        shutil.copy('/home/mlcolab/hzhou52/knowledge_tracing/models/HSSM.py',
-                        global_args.log_path)
-        shutil.copy('/home/mlcolab/hzhou52/knowledge_tracing/models/learner_hssm_vcl_model.py',
-                        global_args.log_path)
-        shutil.copy('/home/mlcolab/hzhou52/knowledge_tracing/VCLRunner.py',
-                        global_args.log_path)
-        shutil.copy('/home/mlcolab/hzhou52/knowledge_tracing/KTRunner.py',
-                        global_args.log_path)
-        
+        shutil.copy(
+            "/home/mlcolab/hzhou52/knowledge_tracing/models/HSSM.py",
+            global_args.log_path,
+        )
+        shutil.copy(
+            "/home/mlcolab/hzhou52/knowledge_tracing/models/learner_hssm_vcl_model.py",
+            global_args.log_path,
+        )
+        shutil.copy(
+            "/home/mlcolab/hzhou52/knowledge_tracing/VCLRunner.py", global_args.log_path
+        )
+        shutil.copy(
+            "/home/mlcolab/hzhou52/knowledge_tracing/KTRunner.py", global_args.log_path
+        )
+
     if global_args.load > 0:
         model.load_model(model_path=global_args.load_folder)
     logs.write_to_log_file(model)
     model.actions_before_train()
-    
+
     # Move to current device
     if torch.cuda.is_available():
         if global_args.distributed:
-            model, _ = utils.distribute_over_GPUs(global_args, model, num_GPU=global_args.num_GPU)
-        else: 
+            model, _ = utils.distribute_over_GPUs(
+                global_args, model, num_GPU=global_args.num_GPU
+            )
+        else:
             model = model.to(global_args.device)
-            
+
     # Running
     if global_args.vcl:
         runner = VCLRunner.VCLRunner(global_args, logs)
@@ -207,5 +231,6 @@ runner.train(model, corpus)
 # logs.write_to_log_file('\nTest After Training: ' + runner._print_res(model, corpus))
 
 # model.module.actions_after_train()
-logs.write_to_log_file(os.linesep + '-' * 45 + ' END: ' + utils.get_time() + ' ' + '-' * 45)
-    
+logs.write_to_log_file(
+    os.linesep + "-" * 45 + " END: " + utils.get_time() + " " + "-" * 45
+)
--- script/predict_learner_performance_baseline.py
+++ script/predict_learner_performance_baseline.py
@@ -1,9 +1,10 @@
 # @Date: 2023/07/25
 
 import sys
-sys.path.append('..')
 
-import os 
+sys.path.append("..")
+
+import os
 import pickle
 import argparse
 import numpy as np
@@ -13,6 +14,7 @@ import torch
 
 from knowledge_tracing.data import data_loader
 from knowledge_tracing.runner import runner_baseline
+
 # from VCLRunner_baseline import BaselineVCLRunner
 # from FTRunner_baseline import FTRunner
 from knowledge_tracing.utils import utils, arg_parser, logger
@@ -20,59 +22,68 @@ from knowledge_tracing.baseline import DKT, DKTForgetting, HKT, AKT, HLR, PPE
 
 # TODO: this is duplicate with the one in exp
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # ----- add aditional arguments for this exp. -----
-    parser = argparse.ArgumentParser(description='Global')
+    parser = argparse.ArgumentParser(description="Global")
 
-    parser.add_argument('--finetune', type=int, default=0)
-    parser.add_argument('--vcl_predict_step', type=int, default=10)
-    parser.add_argument('--start_epoch', type=int, default=0)
-    parser.add_argument('--multi_node', type=int, default=1)
-    
-    parser.add_argument('--id', type=int, default=0)
-    
+    parser.add_argument("--finetune", type=int, default=0)
+    parser.add_argument("--vcl_predict_step", type=int, default=10)
+    parser.add_argument("--start_epoch", type=int, default=0)
+    parser.add_argument("--multi_node", type=int, default=1)
+
+    parser.add_argument("--id", type=int, default=0)
+
     # Define an argument parser for the model name.
-    init_parser = argparse.ArgumentParser(description='Model')
-    init_parser.add_argument('--model_name', type=str, default='CausalKT', help='Choose a model to run.')
+    init_parser = argparse.ArgumentParser(description="Model")
+    init_parser.add_argument(
+        "--model_name", type=str, default="CausalKT", help="Choose a model to run."
+    )
     init_args, init_extras = init_parser.parse_known_args()
-    
+
     # Get the model name from the command-line arguments.
     # Evaluate the model name string as a Python expression to get the corresponding model class.
     model_name = init_args.model_name
-    model = eval('{0}.{0}'.format(model_name))
-    
+    model = eval("{0}.{0}".format(model_name))
+
     # ----- args -----
     # reference:
     # # https://docs.python.org/3/library/argparse.html?highlight=parse_known_args#argparse.ArgumentParser.parse_known_args
     parser = arg_parser.parse_args(parser)
     parser = model.parse_model_args(parser)
-    global_args, extras = parser.parse_known_args() 
+    global_args, extras = parser.parse_known_args()
     global_args.model_name = model_name
     global_args.time = datetime.datetime.now().isoformat()
     global_args.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+
     # ----- random seed -----
     torch.manual_seed(global_args.random_seed)
     torch.cuda.manual_seed(global_args.random_seed)
     np.random.seed(global_args.random_seed)
-    
+
     # ----- log -----
     logs = logger.Logger(global_args)
-    
+
     # ----- data part -----
-    corpus_path = os.path.join(global_args.data_dir, global_args.dataset, 'Corpus_{}.pkl'.format(global_args.max_step))
+    corpus_path = os.path.join(
+        global_args.data_dir,
+        global_args.dataset,
+        "Corpus_{}.pkl".format(global_args.max_step),
+    )
     data = data_loader.DataReader(global_args, logs)
     if not os.path.exists(corpus_path) or global_args.regenerate_corpus:
         data.create_corpus()
-        data.show_columns() 
-    corpus = data.load_corpus(global_args) 
+        data.show_columns()
+    corpus = data.load_corpus(global_args)
 
     # ----- logger information -----
-    log_args = [global_args.model_name, global_args.dataset, str(global_args.random_seed)]
-    logs.write_to_log_file('-' * 45 + ' BEGIN: ' + utils.get_time() + ' ' + '-' * 45)
+    log_args = [
+        global_args.model_name,
+        global_args.dataset,
+        str(global_args.random_seed),
+    ]
+    logs.write_to_log_file("-" * 45 + " BEGIN: " + utils.get_time() + " " + "-" * 45)
     logs.write_to_log_file(utils.format_arg_str(global_args, exclude_lst=[]))
-    
+
     # ----- GPU & CUDA -----
     if global_args.device.type != "cpu":
         if global_args.GPU_to_use is not None:
@@ -83,29 +94,32 @@ if __name__ == '__main__':
         global_args.num_GPU = None
         global_args.batch_size_multiGPU = global_args.batch_size
     logs.write_to_log_file("# cuda devices: {}".format(torch.cuda.device_count()))
-    
+
     # ----- Model initialization -----
-    if 'ls_' or 'ln_' in global_args.train_mode:
+    if "ls_" or "ln_" in global_args.train_mode:
         num_seq = corpus.n_users
-    else: num_seq = 1
-    
+    else:
+        num_seq = 1
+
     model = model(global_args, corpus, logs)
-        
+
     if global_args.load > 0:
         model.load_state_dict(torch.load(global_args.load_folder), strict=False)
     logs.write_to_log_file(model)
     model.actions_before_train()
-    
+
     # Move to current device
     if torch.cuda.is_available():
         if global_args.distributed:
-            model, _ = utils.distribute_over_GPUs(global_args, model, num_GPU=global_args.num_GPU)
-        else: 
+            model, _ = utils.distribute_over_GPUs(
+                global_args, model, num_GPU=global_args.num_GPU
+            )
+        else:
             model = model.to(global_args.device)
-            
+
     # Running
     # TODO: modify the runner
-    if global_args.vcl: 
+    if global_args.vcl:
         runner = VCLRunner(global_args, logs)
     elif global_args.finetune:
         runner = FTRunner(global_args, logs)
@@ -114,5 +128,6 @@ if __name__ == '__main__':
 
 runner.train(model, corpus)
 model.module.actions_after_train()
-logs.write_to_log_file(os.linesep + '-' * 45 + ' END: ' + utils.get_time() + ' ' + '-' * 45)
-    
+logs.write_to_log_file(
+    os.linesep + "-" * 45 + " END: " + utils.get_time() + " " + "-" * 45
+)

a64df4741d05aac5bb52092d14052fd5cbba0e08
hzhou <zzzzzhq@outlook.com>
2023-09-12 14:42:13 +0200

    Remove unused function ConfigDict in utils.

--- knowledge_tracing/utils/utils.py
+++ knowledge_tracing/utils/utils.py
@@ -335,47 +335,6 @@ def distribute_over_GPUs(
     return model, num_GPU
 
 
-class ConfigDict(dict):
-    """Configuration dictionary that allows the `.` access.
-    Example:
-    ```python
-    config = ConfigDict()
-    config.test_number = 1
-    ```
-    The content could be access by
-    ```python
-    print(config.test_number)  # 1 will be returned.
-    ```
-    """
-
-    def __init__(self, *args, **kwargs):
-        super(ConfigDict, self).__init__(*args, **kwargs)
-        for arg in args:
-            if isinstance(arg, dict):
-                for k, v in arg.iteritems():
-                    self[k] = v
-        if kwargs:
-            for k, v in kwargs.iteritems():
-                self[k] = v
-
-    def __getattr__(self, attr):
-        return self.get(attr)
-
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super(ConfigDict, self).__setitem__(key, value)
-        self.__dict__.update({key: value})
-
-    def __delattr__(self, item):
-        self.__delitem__(item)
-
-    def __delitem__(self, key):
-        super(ConfigDict, self).__delitem__(key)
-        del self.__dict__[key]
-
-
 def pad_lst(lst: list, value: int = 0, dtype: type = np.int64) -> np.ndarray:
     """
     Pad a list of lists with a specified value.

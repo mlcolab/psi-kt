dde7b31635ba6bc7908bae58499cca5d272f8431
zhouhq10 <hzhou52@bg-slurmb-login1.novalocal>
2023-10-25 00:29:07 +0200

    Add test_normalize_timestamps

--- tests/test_psikt.py
+++ tests/test_psikt.py
@@ -11,18 +11,18 @@ import numpy as np
 
 import torch
 
-from knowledge_tracing.groupkt import EPS
-from knowledge_tracing.groupkt.groupkt_graph_representation import VarTransformation
-from knowledge_tracing.groupkt.groupkt import AmortizedGroupKT
-from knowledge_tracing.groupkt.GMVAE.gmvae import InferenceNet
-from knowledge_tracing.groupkt.modules import VAEEncoder
+from knowledge_tracing.psikt import EPS
+from knowledge_tracing.psikt.psikt_graph_representation import VarTransformation
+from knowledge_tracing.psikt.psikt import AmortizedPSIKT
+from knowledge_tracing.psikt.GMVAE.gmvae import InferenceNet
+from knowledge_tracing.psikt.modules import VAEEncoder
 from knowledge_tracing.runner.runner import KTRunner
 from knowledge_tracing.utils.logger import Logger
 
 DIM_S = 4
 
 @pytest.fixture
-def groupkt():
+def psikt():
     class args_example(object):
         def __init__(
             self,
@@ -55,7 +55,7 @@ def groupkt():
     
     args = args_example()
     logs = Logger(args)
-    model = AmortizedGroupKT(num_node=num_node, args=args, device=device, logs=logs, nx_graph=nx_graph)
+    model = AmortizedPSIKT(num_node=num_node, args=args, device=device, logs=logs, nx_graph=nx_graph)
     
     return model
 
@@ -74,30 +74,41 @@ def qs_dist():
     qs_dist = torch.distributions.MultivariateNormal(qs_mean, torch.diag_embed(qs_cov_mat))
     return qs_dist
 
+def test_normalize_timestamps(psikt):
+    # Check if the method normalizes timestamps correctly
+    timestamps = torch.tensor([[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]])
+    normalized = psikt._normalize_timestamps(timestamps)
+
+    expected_mean = torch.tensor([[2.5, 3.5, 4.5]])
+    expected_var = torch.tensor([[4.5, 4.5, 4.5]])
+    expected_normalized = (timestamps - expected_mean) / torch.sqrt(expected_var)
+
+    assert torch.allclose(normalized, expected_normalized)
+
 # Test the _init_weights method
-def test_init_weights(groupkt):
+def test_init_weights(psikt):
     # Call the _init_weights method
-    groupkt._init_weights()
+    psikt._init_weights()
 
     # Check that the model attributes are initialized correctly
-    assert isinstance(groupkt.node_dist, VarTransformation)
-    assert isinstance(groupkt.gen_s0_mean, torch.Tensor)
-    assert isinstance(groupkt.gen_s0_log_var, torch.Tensor)
-    assert isinstance(groupkt.gen_z0_mean, torch.Tensor)
-    assert isinstance(groupkt.gen_z0_log_var, torch.Tensor)
-    assert isinstance(groupkt.gen_st_h, torch.Tensor)
-    assert isinstance(groupkt.gen_st_log_r, torch.Tensor)
-    assert isinstance(groupkt.y_emit, torch.nn.Sigmoid)
-    assert isinstance(groupkt.infer_network_emb, torch.nn.Module)
-    assert isinstance(groupkt.infer_network_posterior_s, InferenceNet)  # Replace with the actual class name
-    assert isinstance(groupkt.infer_network_posterior_z, torch.nn.LSTM)
-    assert isinstance(groupkt.infer_network_posterior_mean_var_z, VAEEncoder)  # Replace with the actual class name
+    assert isinstance(psikt.node_dist, VarTransformation)
+    assert isinstance(psikt.gen_s0_mean, torch.Tensor)
+    assert isinstance(psikt.gen_s0_log_var, torch.Tensor)
+    assert isinstance(psikt.gen_z0_mean, torch.Tensor)
+    assert isinstance(psikt.gen_z0_log_var, torch.Tensor)
+    assert isinstance(psikt.gen_st_h, torch.Tensor)
+    assert isinstance(psikt.gen_st_log_r, torch.Tensor)
+    assert isinstance(psikt.y_emit, torch.nn.Sigmoid)
+    assert isinstance(psikt.infer_network_emb, torch.nn.Module)
+    assert isinstance(psikt.infer_network_posterior_s, InferenceNet)  # Replace with the actual class name
+    assert isinstance(psikt.infer_network_posterior_z, torch.nn.LSTM)
+    assert isinstance(psikt.infer_network_posterior_mean_var_z, VAEEncoder)  # Replace with the actual class name
 
 
-def test_construct_univariate_normal(groupkt):
+def test_construct_univariate_normal(psikt):
     mean = torch.tensor([2.0])
     std = torch.tensor([[1.0]])
-    dist = groupkt._construct_normal_from_mean_std(mean, std)
+    dist = psikt._construct_normal_from_mean_std(mean, std)
     
     from torch.distributions import MultivariateNormal
     # Check if the constructed distribution is a univariate normal
@@ -108,13 +119,13 @@ def test_construct_univariate_normal(groupkt):
     assert torch.allclose(dist.covariance_matrix, std.pow(2), atol=EPS)
     
 
-def test_initialize_gaussian_mean_log_var(groupkt):
+def test_initialize_gaussian_mean_log_var(psikt):
     dim = 3
     use_trainable_cov = True
     num_sample = 2
     cov_min = 0.05
     
-    x0_mean, x0_log_var = groupkt._initialize_gaussian_mean_log_var(dim, use_trainable_cov, num_sample)
+    x0_mean, x0_log_var = psikt._initialize_gaussian_mean_log_var(dim, use_trainable_cov, num_sample)
     
     # Check if the shapes of mean and log variance match the expected shapes
     assert x0_mean.shape == (num_sample, dim)
@@ -128,14 +139,14 @@ def test_initialize_gaussian_mean_log_var(groupkt):
     assert torch.allclose(x0_log_var, torch.log(torch.tensor(cov_min)), atol=1e-6)
 
 
-def test_positional_encoding1d(groupkt):
+def test_positional_encoding1d(psikt):
     d_model = 16
     length = 4
     
     actual_time = torch.tensor([[1.0, 2.0, 3.0, 4.0]])
     
     # Call the method with actual_time
-    pe = groupkt._positional_encoding1d(d_model, length, actual_time)
+    pe = psikt._positional_encoding1d(d_model, length, actual_time)
     assert pe.shape == (1, length, d_model)
     assert torch.allclose(pe, torch.zeros_like(pe), atol=1)
     
@@ -151,16 +162,16 @@ def test_positional_encoding1d(groupkt):
     assert torch.allclose(pe, expected_pe, atol=1)
     
 
-def test_st_transition_gen(groupkt, qs_dist):
+def test_st_transition_gen(psikt, qs_dist):
     bs, _, time, _ = qs_dist.mean.shape
     
-    transition_st_h = groupkt.gen_st_h
+    transition_st_h = psikt.gen_st_h
     assert isinstance(transition_st_h, torch.Tensor)
     assert transition_st_h.shape == (DIM_S, DIM_S)
     assert transition_st_h.requires_grad
     
     # Call the st_transition_gen method
-    ps_dist = groupkt.st_transition_gen(qs_dist, eval=False)
+    ps_dist = psikt.st_transition_gen(qs_dist, eval=False)
     assert isinstance(ps_dist, torch.distributions.MultivariateNormal)
     ps_dist_mean = ps_dist.mean
     ps_dist_cov_mat = ps_dist.covariance_matrix
@@ -170,9 +181,9 @@ def test_st_transition_gen(groupkt, qs_dist):
     num_samples = int(1e6)
     qs_sample = qs_dist.sample((num_samples,))  # [num_samples, bs, 1, time, dim_s]
     qs_sample_transition = (
-        qs_sample[:, :, :, :-1] @ groupkt.gen_st_h 
+        qs_sample[:, :, :, :-1] @ psikt.gen_st_h 
     )  # [num_samples, bs, 1, time-1, dim_s]
-    qs_new_dist = torch.distributions.MultivariateNormal(qs_sample_transition, torch.diag_embed(torch.exp(groupkt.gen_st_log_r)+EPS))
+    qs_new_dist = torch.distributions.MultivariateNormal(qs_sample_transition, torch.diag_embed(torch.exp(psikt.gen_st_log_r)+EPS))
     # qs_sample_transition_mean = qs_sample_transition.mean(dim=0)  # [bs, 1, time-1, dim_s]
     # qs_sample_transition_cov_mat = qs_sample_transition.var(dim=0)  # [bs, 1, time-1, dim_s]
 
@@ -183,13 +194,13 @@ def test_st_transition_gen(groupkt, qs_dist):
     # assert new_dist.log_prob(qs_sample_transition_mean).mean() > -1e-1
     
     
-def test_zt_transition_gen(groupkt):
+def test_zt_transition_gen(psikt):
     # Prepare test data
     feed_dict = {"time_seq": torch.tensor([[0.0, 1.0, 2.0], [0.0, 2.0, 4.0]])}  # Replace with appropriate data
     # Prepare other required arguments if necessary
 
     # Call the function
-    pz_dist = groupkt.zt_transition_gen(feed_dict)
+    pz_dist = psikt.zt_transition_gen(feed_dict)
 
     # Add assertions to check if the output is as expected
     assert isinstance(pz_dist, torch.distributions.MultivariateNormal)
